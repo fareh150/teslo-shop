12c6f9e788beda7fd62275fa217ff6b02d3a95f3
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-11 06:19:11 -0400

    conect to orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type:     'postgres',
+      host:     process.env.DB_HOST,
+      port:     +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      autoLoadEntities: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    })
   ],
   controllers: [],
   providers: [],
