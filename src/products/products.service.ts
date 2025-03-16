58b3b2a6d5f45ed271f74cf36ec0d58b20d4da29
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-15 21:33:02 -0400

    find by uuid and slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService
@@ -38,8 +39,17 @@ export class ProductsService
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({id});
+  async findOne(term: string)
+  {
+    let product: Product;
+
+    if (isUUID(term))
+    {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+    
     if (!product)
     {
       throw new BadRequestException('product not found');
