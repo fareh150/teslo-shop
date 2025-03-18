d49bed0462f17e6003fa83f9335cdf8e90212031
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-18 00:34:25 -0400

    add query runner and use

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -118,11 +118,32 @@ export class ProductsService
     }
 
     const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
     try {
-      await this.productRepository.save(product)
+      if (images)
+      {
+        await queryRunner.manager.delete(ProductImage,
+          {
+            product: { id }
+          });
+        
+          product.images = images.map(
+            image => this.productImageRepository.create({ url: image })
+          );
+      } else {
+
+      }
+
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
       return product;
     } catch (error) {
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
       this.handleDBExepctions(error);
     }
   }
