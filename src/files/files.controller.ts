eba165e9b1b8e1bef8f794c4383d9812e543be20
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-20 19:55:47 -0400

    filter usage in UseInterceptors

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,18 +1,24 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file',
+    {
+      fileFilter: fileFilter,
+    }
+  ))
   uploadProductImage(
     @UploadedFile() file: Express.Multer.File
   )
   {
-    // file es lo que se envia a cloudinary para guardarlo fuera del servidor
-    return file;
+    return {
+      fileName: file.originalname,
+    };
   }
 }
