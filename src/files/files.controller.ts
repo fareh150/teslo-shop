39c897a76a50a8c549b49eab855346cfb46e1156
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-20 21:50:06 -0400

    get error

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
@@ -17,6 +17,11 @@ export class FilesController {
     @UploadedFile() file: Express.Multer.File
   )
   {
+    if (!file)
+    {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+    
     return {
       fileName: file.originalname,
     };
