636b3b45d3303b822471de4662a53c645d0c66ed
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-20 23:16:47 -0400

    url usage and creation

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string
+  )
+  {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file',
@@ -28,8 +39,10 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image');
     }
 
+    const secureUrl = `${file.filename}`;
+
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
