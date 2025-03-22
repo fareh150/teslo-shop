c2867839c420ed6d9dfaaa73b01f9ab0c9b3c1b5
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-03-22 00:14:10 -0400

    control error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
@@ -19,7 +19,19 @@ export class AuthService {
 
       return user;
     } catch (error) {
-      console.log(error);
+      this.handleDBErrors(error);
     }
+  }
+
+  private handleDBErrors(error: any): never
+  {
+    if (error.code === '23505')
+    {
+      throw new BadRequestException(error.detail);
+    }
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
