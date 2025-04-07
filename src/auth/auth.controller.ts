ee979766e67d5de8541daa927aa11ba434f067f1
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-04-07 17:58:16 +0200

    create first guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from './entities/user.entity';
 import { GetUser, RawHeaders } from './decorators';
+import { UserRoleGuard } from './guards/user-role.guard';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -35,6 +36,19 @@ export class AuthController {
       user: user,
       userEmail,
       rawHeaders,
+    }
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'superuser'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser() user: User,
+  )
+  {
+    return {
+      ok: true,
+      user: user,
     }
   }
 }
