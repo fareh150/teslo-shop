a14be7f6182fef2baabc4f25602f98c50661986f
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-04-07 15:15:56 +0200

    wip

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } fro
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { GetUser, RawHeaders } from './decorators';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -24,15 +24,17 @@ export class AuthController {
     @Req() req: Express.Request,
     @GetUser() user: User,
     @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
   )
   {
-    console.log('req', req);
+    //console.log('req', req);
     //console.log(user);
     return {
       ok: true,
       message: 'Hello private world',
       user: user,
       userEmail,
+      rawHeaders,
     }
   }
 }
