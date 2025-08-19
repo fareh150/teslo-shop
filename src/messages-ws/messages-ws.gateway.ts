86954f076a2b758c7c9e4ee65c15d6b02ffb5961
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-19 17:13:24 +0200

    detectar cliente conectado y desconectado

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,16 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(
+    private readonly messagesWsService: MessagesWsService
+  ) {}
+  handleConnection(client: any) {
+    console.log('Client connected:', client.id);
+  }
+  handleDisconnect(client: any) {
+    console.log('Client disconnected:', client.id);
+  }
+
 }
