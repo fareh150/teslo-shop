b9be01e2f92526c73598f547c2495f25c9b8dddb
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-19 17:15:10 +0200

    intalmos socket.io y ponemos el tipado del cliente

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,15 +1,16 @@
 import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) {}
-  handleConnection(client: any) {
+  handleConnection(client: Socket) {
     console.log('Client connected:', client.id);
   }
-  handleDisconnect(client: any) {
+  handleDisconnect(client: Socket) {
     console.log('Client disconnected:', client.id);
   }
 
