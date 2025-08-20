23bf65a013a0cad879779ed8a860d0ac4eda1295
Fareh Romero <37407746+fareh150@users.noreply.github.com>
2025-08-20 10:30:11 +0200

    bloquear conexciones con el mismo token

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -28,6 +28,8 @@ export class MessagesWsService {
         if (!user) throw new Error('User not found');
         if (!user.isActive) throw new Error('User is not active');
 
+        this.checkUserConnection(user);
+
         this.connectedClients[client.id] = {
             socket: client,
             user: user
@@ -48,5 +50,17 @@ export class MessagesWsService {
 
     getUserFullName(socketId: string) {
         return this.connectedClients[socketId]?.user.fullName;
+    }
+
+    private checkUserConnection(user: User)
+    {
+        for (const clientId of Object.keys(this.connectedClients)) {
+            const connectedClient = this.connectedClients[clientId];
+            if ( connectedClient.user.id === user.id)
+            {
+                connectedClient.socket.disconnect();
+                break;
+            }
+        }
     }
 }
